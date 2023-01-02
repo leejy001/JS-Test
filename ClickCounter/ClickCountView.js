a7123a68581c68bd6f11e699dbcf2b9a2b4790f8
leejy001 <greatness007@naver.com>
2023-01-02 13:57:01 +0900

    ClickCountView 추가

---
+++ ClickCounter/ClickCountView.js
@@ -0,0 +1,28 @@
+var App = App || {};
+
+App.ClickCountView = (clickCounter, options) => {
+  if (!clickCounter)
+    throw new Error(App.ClickCountView.messages.noClickCounter);
+  if (!options.updateEl)
+    throw new Error(App.ClickCountView.messages.noUpdateEl);
+
+  const view = {
+    updateView() {
+      options.updateEl.innerHTML = clickCounter.getValue();
+    },
+    increaseAndUpdateView() {
+      clickCounter.increase();
+      this.updateView();
+    },
+  };
+  options.triggerEl.addEventListener("click", () => {
+    view.increaseAndUpdateView();
+  });
+
+  return view;
+};
+
+App.ClickCountView.messages = {
+  noClickCounter: "clickCount를 주입해야 합니다",
+  noUpdateEl: "updateEl를 주입해야 합니다",
+};
