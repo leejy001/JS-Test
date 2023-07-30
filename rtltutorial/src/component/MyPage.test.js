c1774fcb828c4488f8ffa8f3deb5cb70f96b1569
leejy001 <junyoung1818@gmail.com>
2023-07-30 15:17:10 +0900

    rtl study - 간단한 테스트 작성

---
+++ rtltutorial/src/component/MyPage.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import MyPage from "./MyPage";
+
+test("유저가 없으면 로그인 문구와 버튼을 보여준다.", () => {
+  render(<MyPage />);
+  const txtEl = screen.getByText(/로그인을 해주세요/);
+  const btnEl = screen.getByRole("button");
+  expect(txtEl).toBeInTheDocument();
+  expect(btnEl).toBeInTheDocument();
+  expect(btnEl).toHaveTextContent("로그인");
+});
+
+test("유저가 있으면 환영문구를 보여준다.", () => {
+  render(<MyPage user={{ name: "KIM" }} />);
+  const txtEl = screen.getByText(/KIM님 환영합니다/);
+  expect(txtEl).toBeInTheDocument();
+});
+
+test("유저가 name이 없으면 로그인 문구와 버튼을 보여준다.", () => {
+  render(<MyPage user="Park" />);
+  const txtEl = screen.getByText(/로그인을 해주세요/);
+  const btnEl = screen.getByRole("button");
+  expect(txtEl).toBeInTheDocument();
+  expect(btnEl).toBeInTheDocument();
+  expect(btnEl).toHaveTextContent("로그인");
+});
